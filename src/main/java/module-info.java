d8afba27cd12ca12883b00a0d7a1f9ccf6ec6eea
sergi <sergio.negro@educa.madrid.org>
2026-03-31 20:54:44 +0200

    Cambios realizados para poder conectar con SpringBoot (sin authentication) - Cambios realizados en sidebar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,9 @@ module org.example.tfgjavafxpruebas {
     requires java.net.http;
     requires com.fasterxml.jackson.databind;
 
-    opens org.example.tfgjavafxpruebas.controller to javafx.fxml;
-
     opens org.example.tfgjavafxpruebas to javafx.fxml;
+    opens org.example.tfgjavafxpruebas.controller to javafx.fxml;
+    opens org.example.tfgjavafxpruebas.model to javafx.base, javafx.controls, javafx.fxml;
+
     exports org.example.tfgjavafxpruebas;
 }
---
+++ src/main/java/org/example/tfgjavafxpruebas/controller/SidebarController.java
@@ -0,0 +1,5 @@
+package org.example.tfgjavafxpruebas.controller;
+
+public class SidebarController extends BaseController {
+    // Hereda todos los métodos de navegación de BaseController
+}
